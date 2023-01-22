f6bf1cbe676c85dac8b69c5a5dc973b5e21b4839
gilvansantos <gilvan_santos19@hotmail.com>
2023-01-22 11:03:29 -0300

    generate

--- nuxt.config.js
+++ nuxt.config.js
@@ -74,7 +74,7 @@ export default {
     // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {
         // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-        baseURL: 'http://192.168.15.4:8000/api/v1/',
+        baseURL: 'https://api.beeu.devg.com.br/api/v1/',
     },
 
     // Build Configuration: https://go.nuxtjs.dev/config-build
